8330e16f32c53ec3e774efe79b57dae9b938bb7f
valentina <valentinacavaliere26@gmail.com>
2025-11-24 11:31:31 -0300

    feira
    finalizando o projeto

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnContinuar = document.getElementById("btnContinuar");
+
+btnContinuar.addEventListener('click', () => {
+    // captura o nome digitado pelo usuario
+    let txtNome = document.getElementById("txtNome").value;
+    // armazena o nome do usuario
+    localStorage.setItem("nomeUsuario", txtNome);
+    // manda para outra pagina
+    location.href = "primeiratela.html";
+});
