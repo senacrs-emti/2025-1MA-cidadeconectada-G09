43340464bace4d548f87c0f3b2e0ea18bf0bdd56
aninha <anamaffe8@hotmail.com>
2025-12-01 12:02:52 -0300

    problemas com codigo JavaScript
    encontrei os segguintes problemas: o mapa some uando adiciono codigo novo, logo fiz outro arquivo pra nao misturar. Oque falta? redirecionar da primeira tela para segunda e utilizar nome da variavel da valentina.

---
+++ mapa.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([-30.025383, -51.212392], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+    
+
--- segundaTela.js
+++ segundaTela.js
@@ -1,25 +1,12 @@
-var map = L.map('map').fitWorld();
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map); 
+       const BotaoBike = document.getElementById("btnBike");
+         if (botaoBike) {
+        
+        botaoBicicleta.addEventListener('click', () => {
+        
+            window.location.href = 'segundatela.html';
+        });
 
-map.locate({setView: true, maxZoom: 16});
-
-function onLocationFound(e) {
-    var radius = e.accuracy;
-
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
-}
-map.on('locationfound', onLocationFound);
-function onLocationError(e) {
-    alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
-
-
+    } else {
+        console.error("Elemento com ID 'btn-bicicleta' não encontrado.");
+    }
